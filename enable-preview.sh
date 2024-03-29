4822187f1c71ba1b44d56462782e995d6d63bbbc
Andrew Stowell <andrew@digitaladapt.com>
2024-03-29 20:05:44 +0000

    no longer releasing elsewhere, so no more preview

--- enable-preview.sh
+++ enable-preview.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-location=`dirname "$0"`
-http-server "${location}/dist" --port 4000
+#location=`dirname "$0"`
+#http-server "${location}/dist" --port 4000
+echo 'dist is the live website, preview will be identical'
 
--- release-update.sh
+++ release-update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-location=`dirname "$0"`
-rsync --archive --progress --delete "${location}/dist/" static-server:/var/www/home
+#location=`dirname "$0"`
+#rsync --archive --progress --delete "${location}/dist/" static-server:/var/www/home
+echo 'dist is the live website, no need to release'
 
